c167af45d493b5e62f51572b360494eec38fa44b
Srijal Joshi <bladedge666@gmail.com>
2016-12-01 01:41:50 +0545

    Setup empty methods for posts_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'about' => "static#about"
-  root 'static#about'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+  resources :posts
 end
