30feeae055f6d341b0792868d5962a062d197751
Srijal Joshi <bladedge666@gmail.com>
2016-12-15 11:40:20 +0545

    Ch 20. Testing the edit action

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -50,4 +50,12 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  describe "GET #edit" do 
+    it "renders the edit template for a single post" do
+      post = FactoryGirl.create(:post)
+      get :edit, params: { id: post.id }
+      expect(response).to render_template :edit
+    end
+  end
+
 end
